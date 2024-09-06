9ad2a807cbee2308be41a85e135805bb6bd2a032
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:25:10 +0200

    Add files via upload

---
+++ Multiplication/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Skriv ditt tal du vill multiplicera : ");
+
+string input = Console.ReadLine();
+
+int number = int.Parse(input);
+
+
+
+for (int i = 1; i <= 10; i++)
+{
+  int result = number * i;
+
+    Console.WriteLine(number + "x" + i + "=" + result);
+}
+
